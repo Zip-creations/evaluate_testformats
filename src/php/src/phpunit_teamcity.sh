2f516b3c8dc86fcd7d7c22fc42b4539355d8cbc2
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2026-06-28 18:51:29 +0200

    fixed file types

--- src/php/src/phpunit_teamcity.sh
+++ src/php/src/phpunit_teamcity.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-phpunit test/phpunit/test.php test/phpunit/test2.php --log-teamcity out/phpunit/report_phpunit_teamcity.xml
+phpunit test/phpunit/test.php test/phpunit/test2.php --log-teamcity out/phpunit/report_phpunit_teamcity.txt
